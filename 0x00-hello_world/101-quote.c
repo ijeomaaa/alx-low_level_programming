68e7160a6dbc967820c89700211902228e0af149
Ijeoma <harahmeeday@gmail.com>
2022-09-08 15:38:36 -0700

    print sentence

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
  *  * main - Prints a sentence
  *   *
  *    * Return: Always 1.
- *     */
+**/
 int main(void)
 {
 		write(2,
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-					      	      57);
+			   "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			      57);
 
 			return (1);
 }
