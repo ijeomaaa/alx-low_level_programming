149ea19118f9f381e4d22e6bf09b51f775b26fc7
Ijeoma <harahmeeday@gmail.com>
2022-09-08 13:47:32 -0700

    print line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-include <stdio.h>
-
+nclude <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * * main-Entry point
+ * * Return: 0 if successful
+ * */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
