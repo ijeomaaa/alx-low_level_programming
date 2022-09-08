1098ca54d28aa0ce4c41575d281e4f6271239f40
Ijeoma <harahmeeday@gmail.com>
2022-09-08 13:11:23 -0700

    print line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+		//string initialisation
+		    char Mystr1[10] = "Programming is like building a multilingual puzzle";	            
+	                puts(Mystr1)
+	     			return 0
+}
+	
